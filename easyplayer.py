60b668ca55500826d06fbc0cb50a73ab24a58e5a
Markus Meskanen <markusmeskanen@gmail.com>
2015-10-28 15:37:40 +0200

    Updated all the docstrings and comments
    Greatly improved docstrings:
    - Much more descriptive of what the function or class does
    - Uses Sphinx!
    - Added parameter explanations
    I've commented almost every line of code and improved the comments in
    general to match the Source.Python's style. I also added an `__all__` declaration.

--- easyplayer.py
+++ easyplayer.py
@@ -1,40 +1,56 @@
-# ======================================================================
-# >> IMPORTS
-# ======================================================================
+# ../packages/custom/easyplayer.py
 
-# Python 3
+"""Provides :class:`EasyPlayer` and :class:`PlayerEffect` classes."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Python 3 Impors
+#   Collections
 from collections import defaultdict
+#   Functools
 from functools import wraps
 
-# Source.Python
+# Source.Python Imports
+#   Entities
 from entities.constants import MoveType
 from entities.constants import TakeDamage
 from entities.helpers import index_from_pointer
 from entities.hooks import EntityPreHook
 from entities.hooks import EntityCondition
-
+#   Events
 from events.listener import _EventListener
-
+#   Listeners
 from listeners import LevelShutdown
 from listeners.tick import tick_delays
-
+#   Players
 from players.entity import PlayerEntity
 from players.helpers import index_from_userid
-
+#   Weapons
 from weapons.entity import WeaponEntity
 
 
-# ======================================================================
-# >> HOOKS
-# ======================================================================
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = (
+    'EasyPlayer',
+    'PlayerEffect',
+)
 
+
+# =============================================================================
+# >> HOOKS
+# =============================================================================
 @EntityPreHook(EntityCondition.is_player, 'bump_weapon')
 def _pre_bump_weapon(args):
-    """
-    Hooked to bump_weapon function to implement weapon restrictions.
-    """
+    """Hooks bump_weapon function to implement weapon restrictions."""
+
+    # Get the player and weapon entities
     player = EasyPlayer(index_from_pointer(args[0]))
     weapon = WeaponEntity(index_from_pointer(args[1]))
+
+    # Return False if the weapon is restricted for the player
     if weapon.classname in player.restrictions:
         return False
 
@@ -45,7 +61,12 @@ _original_fire_game_event = _EventListener.fire_game_event
 
 @wraps(_original_fire_game_event)
 def _fire_game_event(self, game_event):
+    """Custom fire_game_event method for post listeners."""
+
+    # Call the original fire_game_event normally
     _original_fire_game_event(self, game_event)
+
+    # Call the post functions
     event_name = game_event.get_name()
     if event_name == 'player_death':
         _post_player_death(game_event)
@@ -57,15 +78,11 @@ def _fire_game_event(self, game_event):
 _EventListener.fire_game_event = _fire_game_event
 
 
-# ======================================================================
+# =============================================================================
 # >> GAME EVENTS
-# ======================================================================
-
+# =============================================================================
 def _post_player_death(game_event):
-    """
-    Cancel any active player effects.
-    Also resets player's gravity (Source engine pls).
-    """
+    """Cancel any active player effects and reset player's gravity."""
     player = EasyPlayer.from_userid(game_event.get_int('userid'))
     player.gravity = 1.0
     player.restrictions.clear()
@@ -73,10 +90,7 @@ def _post_player_death(game_event):
 
 
 def _post_player_disconnect(game_event):
-    """
-    Clean up the EasyPlayer instance from all EasyPlayer (sub)classes.
-    Also cancels all effects from the disconnecting player.
-    """
+    """Clean up the players from all EasyPlayer classes."""
     index = index_from_userid(game_event.get_int('userid'))
     player = EasyPlayer.from_userid(game_event.get_int('userid'))
     player.cancel_all_effects()
@@ -85,157 +99,195 @@ def _post_player_disconnect(game_event):
 
 @LevelShutdown
 def level_shutdown():
-    """
-    Clean up all EasyPlayer instances from all EasyPlayer (sub)classes.
-    """
+    """Clean up all players from all EasyPlayer classes."""
     _EasyPlayerMeta.discard_all_players()
 
 
-# ======================================================================
+# =============================================================================
 # >> PLAYER EFFECTS
-# ======================================================================
-
+# =============================================================================
 class _PlayerEffect(object):
-    """
-    Class for player effects like freeze() and burn().
-    """
+    """Class for player effects like freeze() and burn()."""
 
     def __init__(self, descriptor_obj, player):
-        """
-        Initialize a new effect for a player.
-        `descriptor_obj` should be of type `PlayerEffect`.
+        """Initialize a new effect for a player.
+
+        :param descriptor_obj: The :class:`PlayerEffect` instance
+        :param player: Player who to apply the effect on
         """
         self._descriptor_obj = descriptor_obj
         self._player = player
         self._delay = None
 
     def _enable(self, duration=None):
+        """Enable the effect.
+
+        :param duration: Duration after which to cancel the effect
         """
-        Enable the effect.
-        If `duration` is a positive integer, cancel the effect
-        automatically after the duration has ended.
-        """
+
+        # If the duration is a positive integer
         if isinstance(duration, int) and duration > 0:
+
+            # Start a delay which will disable the effect
             self._delay = tick_delays.delay(duration, self._disable)
+
+        # Add the effect to the player's effects
         self._player._effects[self._descriptor_obj].append(self)
+
+        # Call the descriptor object's on-function on the player
         self._descriptor_obj._on_f(self._player)
-        return self  # For __call__
+
+        # Return the player effect to allow __call__ and .cancel()
+        return self
 
     __call__ = _enable
 
     def _disable(self):
-        """
-        Disable the effect.
-        """
+        """Disable the effect."""
+
+        # Remove the effect from the player's effects
         self._player._effects[self._descriptor_obj].remove(self)
+
+        # If the player has no more effects of this type
         if not self._player._effects[self._descriptor_obj]:
+
+            # Call the descriptor o bject's off-function on the player
             self._descriptor_obj._off_f(self._player)
 
     def cancel(self):
-        """
-        Cancel an effect.
-        Cancels the tick_delay and disables the effect.
-        """
+        """Cancel the tick_delay and disable the effect."""
+
+        # If there is a delay object
         if self._delay is not None:
+
+            # Cancel it
             tick_delays.cancel_delay(self._delay)
             self._delay = None
+
+        # Disable the effect
         self._disable()
 
 
 class PlayerEffect(object):
-    """
-    Decorator similar to `property()` for creating player effects.
-    """
+    """Decorator similar to :class:`property()` for player effects."""
 
     def __init__(self, on_f=None, off_f=None):
-        """
-        Initialize a player effect with the given functions.
+        """Initialize a player effect with the given functions.
+
+        :param on_f: Function to call when the effect starts
+        :param off_f: Function to cal when the effect ends
         """
         self._on_f = on_f
         self._off_f = off_f
 
     def on(self, on_f):
-        """
-        Decorator to add an on_f function to the effect class.
+        """Decorator to add an on_f function to the effect.
+
+        :param on_f: Function to call when the effect starts
         """
         return type(self)(on_f, self._off_f)
 
     def off(self, off_f):
-        """
-        Decorator to add an off_f function to the effect class.
+        """Decorator to add an off_f function to the effect.
+
+        :param off_f: Function to call when the effect ends
         """
         return type(self)(self._on_f, off_f)
 
     def __get__(self, obj, objtype=None):
+        """Get an instance of :class:`_PlayerEffect`.
+
+        :param obj: The object who's accessing the effect
+        :param objtype: Type of the object
         """
-        Return an instance of the effect class.
-        """
+
+        # Return the :class:`PlayerEffect` instance
+        # when accessed through the class
         if obj is None:
             return self
+
+        # Else return a new :class:`_PlayerEffect` instance
         return _PlayerEffect(self, obj)
 
 
-# ======================================================================
+# =============================================================================
 # >> EASY PLAYER
-# ======================================================================
+# =============================================================================
 
 class _EasyPlayerMeta(type(PlayerEntity)):
-    """
-    Metaclass for the EasyPlayer class (and subclasses).
-    Manages all the instances of EasyPlayer, making sure they get
-    cached properly.
+    """Metaclass for the :class:`EasyPlayer` and its subclasses.
 
-    Together with the game events defined in easyplayer.py, this will
-    also clean up players leaving the server or changing the map.
+    Manages all the instances of :class:`EasyPlayer`,
+    making sure they get cached properly.
+
+    Together with the event callbacks defined in :mod:`easyplayer`,
+    this will also clean up players upon disonnect or map change.
     """
 
-    _classes = {}  # In case anyone wants to subclass EasyPlayer
+    # Dictionary to store all the instances of all classes,
+    # in case anyone wants to subclass :class:`EasyPlayer`.
+    _classes = {}
 
     def __init__(cls, *args, **kwargs):
+        """Initialize a new class.
+
+        Creates an instance dictionary for the new class into
+        :attr:`_classes` dictionary.
         """
-        Create a new instance dict for the class into _classes dict.
-        """
-        super().__init__()
         _EasyPlayerMeta._classes[cls] = {}
 
     def __call__(cls, index, *args, **kwargs):
+        """Instantiates the class.
+
+        If an instance with the index already exists, return it instead.
         """
-        Instantiates an EasyPlayer class, but if an instance with
-        the index already exists, return it instead.
-        """
+
+        # Get the instance dictionary for the class
         instances = _EasyPlayerMeta._classes[cls]
+
+        # If there is no instance with such index
         if index not in instances:
+
+            # Normally create a new instance and cache it
             instances[index] = super().__call__(index, *args, **kwargs)
+
+        # Else return the existing cached instance
         return instances[index]
 
     @staticmethod
     def discard_player(player_index):
-        """
-        Discards a player, removing him from all EasyPlayer classes.
-        """
+        """Discard a player, removing him from all classes."""
+
+        # Loop through all of the instance dictionaries
         for instances in _EasyPlayerMeta._classes.values():
+
+            # If an instance of the player exist in the dictionary
             if player_index in instances:
+
+                # Remove him from the dict
                 del instances[player_index]
 
     @staticmethod
     def discard_all_players():
-        """
-        Discards all players, completely clearing the instances.
-        """
+        """Discard all players."""
+
+        # Loop through all instance dictionaries and clear them
         for instances in _EasyPlayerMeta._classes.values():
             instances.clear()
 
 
 class EasyPlayer(PlayerEntity, metaclass=_EasyPlayerMeta):
-    """
-    Custom `PlayerEntity` class with bonus player effects.
-    Also implements restriction system and `from_userid` classmethod.
+    """Custom :class:`PlayerEntity` class with bonus player effects.
+
+    Also implements restrictions and :meth:`from_userid` classmethod.
     """
 
     def __init__(self, index):
-        """
-        Initializes an EasyPlayer instance, adding _effects dict
-        and a set of restricted items.
+        """Initializes an EasyPlayer instance.
+
+        Adds :attr:`_effects` dictionary and :attr:`restrictions` set.
+
+        :param index: Index of the player who to instantiate
         """
         super().__init__(index)
         self._effects = defaultdict(list)
@@ -243,35 +295,50 @@ class EasyPlayer(PlayerEntity, metaclass=_EasyPlayerMeta):
 
     @classmethod
     def from_userid(cls, userid):
-        """
-        Returns an EasyPlayer instance from an userid.
+        """Constructor to get an instance from an userid.
+
+        :param userid: UserID of the player who to instantiate
         """
         return cls(index_from_userid(userid))
 
     def cancel_all_effects(self):
-        """
-        Cancel all the effects of an EasyPlayer instance.
-        """
+        """Cancel all the effects from a player."""
+
+        # Loop through all the lists of player effects
         for effect_list in self._effects.values():
+
+            # Loop through and cancel every effect in the list
             for effect in effect_list:
                 effect.cancel()
+
+        # Clear the :attr:`_effects` dictionary
         self._effects.clear()
 
     def shift_property(self, prop_name, shift, duration=None):
+        """Shifts player's property's value.
+
+        :param prop_name: Name of the property to shift, e.g. 'health'
+        :param shift: Amount of shift to make, can be negative
+        :param duration: Revert the shift after the duration has ended
         """
-        Shifts an EasyPlayer instances property's value for a duration.
-        If duration is a positive integer, revert the shift
-        automatically after the duration has ended.
-        """
+
+        # Get the current value of the property
         old_value = getattr(self, prop_name)
+
+        # Shift the property's value by the shift amount
         setattr(self, prop_name, old_value + shift)
+
+        # If the duration is a positive integer
         if isinstance(duration, int) and duration > 0:
+
+            # Start a delay which will revert the shift
             tick_delays.delay(self.shift_property, prop_name, -shift)
 
     def _update_move_type(self):
-        """
-        Updates the player's move_type attribute to match his latest
-        move type player effects.
+        """Update player's :attr:`move_type` to his player effects.
+
+        This gets called whenver a movement effect starts or ends.
+        Priority order: noclip > freeze > fly > walk
         """
         if self._effects[type(self).noclip]:
             self.move_type = MoveType.NOCLIP
