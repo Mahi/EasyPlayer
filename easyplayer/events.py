5743e4855f6efd4731d16a06c3e0b0e620233025
Mahi <markusmeskanen@gmail.com>
2021-12-14 09:37:28 +0200

    Customization of listened GameEvents

--- easyplayer/events.py
+++ easyplayer/events.py
@@ -4,13 +4,17 @@ from events.manager import event_manager
 __all__ = (
     'Event',
     'EventManager',
-    'DUO_PLAYER_GAME_EVENTS',
-    'SOLO_PLAYER_GAME_EVENTS',
+    'DEFAULT_PLAYER_GAME_EVENTS',
+    'DEFAULT_TWO_PLAYER_GAME_EVENT_MAP',
 )
 
+DEFAULT_PLAYER_GAME_EVENTS = {
+    'player_jump',
+    'player_spawn',
+}
 
-SOLO_PLAYER_GAME_EVENTS = {'player_spawn', 'player_jump'}
-DUO_PLAYER_GAME_EVENT_MAP = {
+DEFAULT_TWO_PLAYER_GAME_EVENT_MAP = {
+    'player_blind': ('player_blind', 'player_go_blind'),
     'player_death': ('player_kill', 'player_death'),
     'player_hurt': ('player_attack', 'player_victim'),
 }
@@ -61,46 +65,85 @@ class EventManager(AutoUnload):
     """Manage both GameEvents and custom events in a uniform manner.
 
     Simply provide your PlayerDictionary as an argument,
-    and all the GameEvent's userids will be converted to your
+    and have the GameEvent's userids be converted to your
     custom Player entities automatically:
 
     >>> player_dict = PlayerDictionary(MyCustomPlayer)
-    >>> events = easyevents.EventManager(player_dict)
-    >>> @events.on('player_kill')
-    ... def on_player_kill(player, victim, **event_args):
+    >>> events = EventManager(player_dict)
+    >>> @events.on('player_jump')
+    ... def on_player_jump(player, **eargs):
     ...     assert type(player) == MyCustomPlayer
-    ...     assert type(victim) == MyCustomPlayer
 
-    Other features include:
-    - Event args being forwared with Python's `**dict` unpacking syntax,
-      allowing you to cherry-pick which arguments you need.
-    - player_death being split up into player_kill and player_death
-    - player_hurt being split up into player_attack and player_victim
-    - Adding custom events with the `create_event()` method
+    You can also provide custom GameEvent names if the defaults
+    aren't suitable for your needs:
+
+    >>> event_names = {'enter_bombzone', 'exit_bombzone'}
+    >>> events = EventManager(player_dict, event_names)
+    >>> @events.on('enter_bombzone')
+    ... def on_enter_bombzone(player, **eargs):
+    ...     assert type(player) == MyCustomPlayer
+
+    By default, all the GameEvents with an attacker and
+    a victim have been split up into two events:
+
+    - player_blind => player_blind and player_go_blind
+    - player_death => player_kill and player_death
+    - player_hurt => player_attack and player_victim
+
+    These can be disabled or modified through the optional
+    `two_player_game_event_map` keyword argument:
+
+    >>> my_two_player_game_event_map = {
+    ...     # game_event: (attacker_event, victim_event),
+    ...     'player_death': ('player_kill', 'player_killed'),
+    ...     # Not providing other events disables them!
+    ... }
+    >>> events = EventManager(
+    ...     player_dict,
+    ...     two_player_game_event_map=my_two_player_game_event_map
+    ... )
+
+    Finally, you can add custom events with `create_event()` and
+    fire them using the ´[]` syntax:
+
+    >>> events = EventManager(player_dict)
+    >>> events.create_event('my_custom_event')
+    >>> @events.on('my_custom_event')
+    ... def on_my_custom_event(**eargs):
+    ...     print(eargs)
+    ...
+    >>> events['my_custom_event'].fire(name=player.name, foo='bar')
+    {'name': 'Mahi', 'foo': 'bar'}
     """
 
-    def __init__(self, player_dict):
+    def __init__(self,
+        player_dict,
+        player_game_events=DEFAULT_PLAYER_GAME_EVENTS,
+        two_player_game_event_map=DEFAULT_TWO_PLAYER_GAME_EVENT_MAP,
+    ):
         """Initialize the event manager for a player dict."""
         self.player_dict = player_dict
         self._events = {}
+        self.player_game_events = player_game_events
+        self.two_player_game_event_map = two_player_game_event_map
 
-        for game_event in SOLO_PLAYER_GAME_EVENTS:
-            event_manager.register_for_event(game_event, self._on_solo_player_game_event)
+        for game_event in self.player_game_events:
+            event_manager.register_for_event(game_event, self._on_player_game_event)
             self.create_event(game_event)
 
-        for game_event, easy_events in DUO_PLAYER_GAME_EVENT_MAP.items():
-            event_manager.register_for_event(game_event, self._on_duo_player_game_event)
+        for game_event, easy_events in self.two_player_game_event_map.items():
+            event_manager.register_for_event(game_event, self._on_two_player_game_event)
             attacker_event, victim_event = easy_events
             self.create_event(attacker_event)
             self.create_event(victim_event)
 
     def _unload_instance(self):
         """Automatically unload the GameEvent listeners."""
-        for event_name in SOLO_PLAYER_GAME_EVENTS:
-            event_manager.unregister_for_event(event_name, self._on_solo_player_game_event)
+        for event_name in self.player_game_events:
+            event_manager.unregister_for_event(event_name, self._on_player_game_event)
 
-        for event_name in DUO_PLAYER_GAME_EVENT_MAP.keys():
-            event_manager.unregister_for_event(event_name, self._on_duo_player_game_event)
+        for event_name in self.two_player_game_event_map.keys():
+            event_manager.unregister_for_event(event_name, self._on_two_player_game_event)
 
     def __getitem__(self, key):
         return self._events[key]
@@ -124,15 +167,15 @@ class EventManager(AutoUnload):
             return listener
         return decorator
 
-    def _on_solo_player_game_event(self, game_event):
-        """Invoke easyevent for a solo player GameEvent."""
+    def _on_player_game_event(self, game_event):
+        """Invoke easyevent for a single player GameEvent."""
         event_args = game_event.variables.as_dict()
         userid = event_args.pop('userid')
         player = self.player_dict.from_userid(userid)
         self[game_event.name].fire(event_args, player=player)
 
-    def _on_duo_player_game_event(self, game_event):
-        """Invoke easyevents for a duo player GameEvent."""
+    def _on_two_player_game_event(self, game_event):
+        """Invoke easyevents for a two player GameEvent."""
         event_args = game_event.variables.as_dict()
         try:
             attacker_id = event_args.pop('attacker')
@@ -144,7 +187,7 @@ class EventManager(AutoUnload):
         victim = self.player_dict.from_userid(userid)
 
         event_args.update(attacker=attacker, victim=victim)
-        attacker_event, victim_event = DUO_PLAYER_GAME_EVENT_MAP[game_event.name]
+        attacker_event, victim_event = self.two_player_game_event_map[game_event.name]
         self[victim_event].fire(event_args, player=victim)
         if attacker is not None:
             self[attacker_event].fire(event_args, player=attacker)
